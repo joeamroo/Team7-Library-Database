2a94d58976ff8d2292131dfe39b298d48cc7e4cb
Gabyc5 <105693076+Gabyc5@users.noreply.github.com>
2024-03-12 13:01:55 -0500

    Catalog Sidebar
    Made sidebar dynamic and added dropdown arrows

--- front-end/catalog.js
+++ front-end/catalog.js
@@ -1,7 +1,7 @@
 function adjustSidebarHeight() {
     var sidebar = document.querySelector('.sidebar');
     var dropdowns = document.getElementsByClassName("dropdown-content");
-    var totalHeight = 175;
+    var totalHeight = 325;
 
     for (var i = 0; i < dropdowns.length; i ++) {
         if (dropdowns[i].classList.contains('show')) {
@@ -17,10 +17,18 @@ function adjustSidebarHeight() {
 function toggleDropdown(dropdownId) {
     var dropdownContent = document.getElementById(dropdownId);
     var dropdownButton = dropdownContent.previousElementSibling;
+    var icon = dropdownButton.querySelector(".icon")
 
     if (event.target === dropdownButton) {
         dropdownContent.classList.toggle("show");
         adjustSidebarHeight();
+
+        if (dropdownContent.classList.contains("show")) {
+            icon.innerHTML = "&#x25B2;";
+        }
+        else {
+            icon.innerHTML = "&#x25BC;";
+        }
     }
         
 }
