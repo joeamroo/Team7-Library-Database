2c5b737966794823de94bbfd48982a1f1705faae
Gabyc5 <105693076+Gabyc5@users.noreply.github.com>
2024-03-13 17:17:48 -0500

    Catalog Continued
    added limit to the amount of results displayed in the page

--- front-end/catalog.js
+++ front-end/catalog.js
@@ -35,16 +35,54 @@ function toggleDropdown(dropdownId) {
 }
 
 
+
+// Setting the catalog container based on how many catalog items the user wants to display 
 function setCatalogContHeight() {
     const catalogContainer = document.querySelector('.catalog-container');
-    const catalogResults = document.querySelector('.catalog-results');
-    const itemHeight = 275;
-    const itemCount = catalogResults.children.length;
-    const minHeight = 275; 
+    const catalogItems = document.querySelectorAll('.catalog-item[style="display: flex;"]');
+    const itemHeight = 260; // Height of each catalog item
+    const minHeight = 260; // Minimum height of the catalog container
 
-    const containerHeight = Math.max(minHeight, itemHeight *itemCount);
+    const containerHeight = Math.max(minHeight, itemHeight * catalogItems.length);
 
     catalogContainer.style.height = containerHeight + 'px';
 }
-setCatalogContHeight();
+
+
+function updateItemsShownOnPage(limit) {
+    const catalogItem = document.querySelectorAll('.catalog-item');
+
+    catalogItem.forEach((item, index) => {
+        if (index < limit) {
+            item.style.display = 'flex';
+        }
+        else {
+            item.style.display = 'none'
+        }
+    });
+}
+
+function loadMoreItems(limit) {
+    const catalogItems = document.querySelectorAll('.catalog-item');
+    const currItemCount = catalogItems.length;
+    const totalItemCount = catalogItems.length;
+
+    const remainingItems = totalItemCount - currItemCount;
+    const itemsToShow = Math.min(remainingItems, limit - currItemCount);
+
+    for (let i = currItemCount; i < currItemCount+itemsToShow; i++) {
+        catalogItems[i].style.display = 'flex';
+    }
+    setCatalogContHeight();
+}
+
+updateItemsShownOnPage(3);
+
+document.getElementById('limit-select').addEventListener('change', function() {
+    const limit = parseInt(this.value);
+
+    updateItemsShownOnPage(limit);
+    loadMoreItems(limit);
+    setCatalogContHeight();
+});
 
