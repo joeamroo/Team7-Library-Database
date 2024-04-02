76c4768fed4b80e893ccd6a44e5e9fda480ef61c
Gabyc5 <105693076+Gabyc5@users.noreply.github.com>
2024-04-02 00:05:49 -0500

    Update returnItems.js

--- server/routes/returnItems.js
+++ server/routes/returnItems.js
@@ -2,7 +2,7 @@ const mysql = require('mysql');
 
 require('dotenv').config();
 const twilio = require('twilio');
-const accountSid = process.env.TWILIO_ACCOUNT_SID;;
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_AUTH_TOKEN;
 const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 const client = twilio(accountSid, authToken);
