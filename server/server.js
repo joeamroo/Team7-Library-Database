2d8b68548bd105f555ce19a95c0dee6faee11a98
Gabyc5 <105693076+Gabyc5@users.noreply.github.com>
2024-04-06 01:32:46 -0500

    Fix Env vars continued

--- server/server.js
+++ server/server.js
@@ -1,6 +1,6 @@
 const http = require('http');
 require('dotenv').config();
-console.log(process.env); 
+console.log(process.env.EXAMPLE_VAR); 
 
 
 const { getInitialCatalogInfo, getCatalogSearchWithRestrictions, insertDataToDatabase } = require('./routes/catalog');
