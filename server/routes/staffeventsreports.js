1cbe20a384a2743e50a8a1161738b67f43b15db4
joeamroo <tempstarz2001@gmail.com>
2024-04-09 01:39:03 -0500

    idk

--- server/routes/staffeventsreports.js
+++ server/routes/staffeventsreports.js
@@ -14,7 +14,7 @@ connection.connect((err) => {
 });
 
 
-
+// get the attendance and signups of this event
 function getEventAttendance(response) {
     connection.query('SELECT * FROM staff_reports', (err, results) => {
         if (err) {
@@ -33,7 +33,27 @@ function getEventAttendance(response) {
     });
 
 }
+// Get the attendance of other events, averaged.
+function otherEventsAttendance(response) {
+    connection.query('SELECT * FROM staff_reports', (err, results) => {
+        if (err) {
+            console.error('Error querying catalog data:', err);
+            response.writeHead(500);
+            response.end('Server error');
+            return;
+        }
 
+        let reportsHtml = '';
+        
+        results.forEach(item => { reportsHtml += createReportHtml(item); });
+        
+        response.writeHead(200, { 'Content-Type': 'text/html' });
+        response.end(reportsHtml, 'utf-8');
+    });
+
+
+}
+// main report to be sent to the staff
 function getEventReports(response) {
     connection.query('SELECT * FROM staff_reports', (err, results) => {
         if (err) {
