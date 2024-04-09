9742ddd0fe3f4498c3f0cda7c2edeea19ce3a40d
Gabyc5 <105693076+Gabyc5@users.noreply.github.com>
2024-04-08 20:23:14 -0500

    Updated Forgot Password Logic

--- front-end/Forgot-pwd/forgot-pwd.js
+++ front-end/Forgot-pwd/forgot-pwd.js
@@ -14,6 +14,7 @@ document.getElementById('reset-sub').addEventListener('click', function(event) {
         const user_id = document.getElementById('userID').value.trim();
         const email = document.getElementById('email').value.trim();
         const new_password = document.getElementById('newPwd').value.trim();
+        const alert = document.getElementById('alert-msg');
         
         const xhr = new XMLHttpRequest();
         xhr.open('POST', resetPasswdUrl); 
@@ -22,9 +23,15 @@ document.getElementById('reset-sub').addEventListener('click', function(event) {
         xhr.onload = function() {
             if (xhr.status === 200) {
                 console.log('sucessfully changed password');
+                document.getElementById('alert-msg').textContent = 'Successfully changed password!';
+                alert.style.visibility = 'visible';
+                alert.style.color = 'green';
             } 
             else if (xhr.status === 409) {
                 console.log('user not found');
+                alert.textContent = 'User not found!';
+                alert.style.visibility = 'visible';
+                alert.style.color = 'red';
             }
             else {
                 console.error('Error :', xhr.statusText);
@@ -44,4 +51,3 @@ document.getElementById('reset-sub').addEventListener('click', function(event) {
         }, 500);
     }
 });
-
