36d4d2296e5cac9050c34d2fab9fccf3693fee84
Gabyc5 <105693076+Gabyc5@users.noreply.github.com>
2024-04-10 13:40:44 -0500

    Update adminEventManagement.js

--- server/routes/adminEventManagement.js
+++ server/routes/adminEventManagement.js
@@ -42,7 +42,7 @@ function getEventsForAdmin(res) {
 function insertEvent(res, name, des, img, sponsor, date, normalizedStartTime, stPeriod, normalizedEndTime, endPeriod) {
     connection.query(`INSERT INTO staff (event_name, event_description, event_img, date, start_time, startAMPM, end_time, endAMPM, event_status, sponsor) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)`, [name, des, img, date, normalizedStartTime, stPeriod, normalizedEndTime, endPeriod, 'active', sponsor ], (newEventErr, result) => {
         if (newEventErr) {
-            console.log('error entering new event into librarydev db:', newMemErr);
+            console.log('error entering new event into librarydev db:', newEventErr);
         }
     });
 
