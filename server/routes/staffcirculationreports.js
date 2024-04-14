95e2bb7b00bb41309a4090f76fb81371dd3a7357
joeamroo <tempstarz2001@gmail.com>
2024-04-14 16:58:54 -0500

    ...

--- server/routes/staffcirculationreports.js
+++ server/routes/staffcirculationreports.js
@@ -13,3 +13,42 @@ connection.connect((err) => {
     console.log('Connected to LibraryDev database');
 });
 
+function buildWhereClause(filters) {
+    const conditions = [];
+  
+    if (filters.name) {
+      conditions.push(`m.name LIKE '%${filters.name}%'`);
+    }
+  
+    if (filters.memberId) {
+      conditions.push(`m.member_id = '${filters.memberId}'`);
+    }
+  
+    if (filters.hasFines) {
+      conditions.push('m.fine > 0');
+    }
+  
+    if (filters.noTransactions) {
+      conditions.push('t.transaction_id IS NULL');
+    }
+  
+    if (conditions.length > 0) {
+      return 'WHERE ' + conditions.join(' AND ');
+    }
+  
+    return '';
+  }
+
+  function getMemberData(filters, callback) {
+    const whereClause = buildWhereClause(filters);
+    const query = `SELECT m.member_id, m.name, m.email, m.phone_number, m.state, m.city_addr, m.street_addr, m.zipcode_addr, m.fine, t.transaction_id, t.date_created, t.due_date, t.return_date
+    FROM member m
+    LEFT JOIN transaction t ON m.member_id = t.member_id
+    ${whereClause}
+    `;
+
+  }
+  
+  
+  module.exports = { getMemberData };
+  
