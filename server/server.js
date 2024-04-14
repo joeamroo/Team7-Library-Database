1b171ba47fcc629e2d3bcc5fba16bc3befb32035
Ben Cornick <ben@cornick.net>
2024-04-13 19:44:45 -0500

    add route to server folder

--- server/server.js
+++ server/server.js
@@ -15,6 +15,7 @@ const { getEventReports } = require ('./routes/staffeventsreports');
 const { getEmployees, insertStaff, removeStaff, updateStaffRole, filterStaff } = require ('./routes/adminStaffManagement');
 const { getEventsForAdmin, insertEvent, deleteEvent, filterEvents } = require('./routes/adminEventManagement');
 const { getItemsForAdmin, filterCatalogItems, getAdminInfo } = require('./routes/adminCatalogManagement');
+const { addItems } = require('./routes/add-items');
 
 function setCorsHeaders(res) {
     res.setHeader('Access-Control-Allow-Origin', '*');
@@ -441,6 +442,22 @@ const server = http.createServer((request, res) => {
                     try {
                         const postData = JSON.parse(body);
                         getAdminInfo(res, postData.staffId);
+                    } 
+                    catch (error) {
+                        console.error('Error parsing JSON:', error);
+                        serve404(res);
+                    }
+                });
+            }
+            else if (pathname === '/addItems') {
+                let body = '';
+                request.on('data', (chunk) => {
+                    body += chunk.toString();
+                });
+                request.on('end', () => {
+                    try {
+                        const postData = JSON.parse(body);
+                        addItems(res, postData.itemType,postData.title,postData.authorDirector,postData.isbn,postData.category,postData.publisherProducer,postData.publicationReleaseDate,postData.imageLink);
                     } 
                     catch (error) {
                         console.error('Error parsing JSON:', error);
